0cea06185121dbb9d5c570dc817bca0fb97323ec
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-08-19 10:58:50 +0200

    mqtt test succesfull

--- river-monitoring-service/waterLevelInterface/views.py
+++ river-monitoring-service/waterLevelInterface/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from .mqtt import client as mqtt_client
 
-# Create your views here.
+
+def publish_message(request):
+    request_data = json.loads(request.body)
+    rc, mid = mqtt_client.publish(request_data['topic'], request_data['msg'])
+    return JsonResponse({'code': rc})
