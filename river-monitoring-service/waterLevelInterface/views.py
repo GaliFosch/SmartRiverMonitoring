902652a197329b8f5617f3ca2f80ef6de313a039
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-11-09 22:11:45 +0100

    created the view that returns the measurments of the last hour

--- river-monitoring-service/waterLevelInterface/views.py
+++ river-monitoring-service/waterLevelInterface/views.py
@@ -1,9 +1,25 @@
+from datetime import timedelta
+from django.utils import timezone
 import json
 from django.http import JsonResponse
 from .mqtt import client as mqtt_client
+from models import Measurment
 
 
 def publish_message(request):
     request_data = json.loads(request.body)
     rc, mid = mqtt_client.publish(request_data['topic'], request_data['msg'])
     return JsonResponse({'code': rc})
+
+def recentMeasurments(request):
+    oneHourAgo = timezone.now() - timedelta(hours=1)
+    recentMeasurments = Measurment.objects.filter(timestamp__gte=oneHourAgo)
+    data = [
+        {
+            "value": measurment.value,
+            "timestamp": measurment.timestamp
+        }
+        for measurment in recentMeasurments
+    ]
+
+    return JsonResponse(data, safe=False)
