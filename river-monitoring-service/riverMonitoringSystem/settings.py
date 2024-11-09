a7a2ae4bac6d50bed330a99280ac9021ef27b77e
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-11-09 21:56:33 +0100

    migrated misurations

--- river-monitoring-service/riverMonitoringSystem/settings.py
+++ river-monitoring-service/riverMonitoringSystem/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'waterLevelInterface.apps.WaterlevelinterfaceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
