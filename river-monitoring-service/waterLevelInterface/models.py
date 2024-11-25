90da9bc76577ca379546fbcee890698926d6424e
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2024-11-25 16:03:23 +0100

    actually removed milliseconds from datetime in database

--- river-monitoring-service/waterLevelInterface/models.py
+++ river-monitoring-service/waterLevelInterface/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Measurement(models.Model):
     value = models.FloatField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField()
+    
+    def save(self, *args, **kwargs):
+    # Remove milliseconds
+        self.timestamp = datetime.datetime.now(tz=timezone.utc).replace(microsecond=0)
+        super(Measurement, self).save(*args, **kwargs)
