a1846b5a91b0a0cbf3dd6608cd2ad4e0e6fd882c
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2024-11-26 17:34:37 +0100

    fixed minor mistake, added request to js file

--- river-monitoring-dashboard/js/waterLevelChart.js
+++ river-monitoring-dashboard/js/waterLevelChart.js
@@ -1,4 +1,5 @@
-const waterLevelIndex = ""
+const waterLevelIndex = "http://127.0.0.1:8000/wtld/get-wlevels/"
+
 document.addEventListener("DOMContentLoaded", ()=>{
     const ctx = document
         .getElementById('waterLevelChart')
@@ -38,25 +39,30 @@ document.addEventListener("DOMContentLoaded", ()=>{
     });
 
     function fetchData() {
-        fetch(waterLevelIndex)
-            .then(response => response.json()) 
-            .then(newData => {
+        let xhttp = new XMLHttpRequest()
+        xhttp.onload = function () {
+            let response = JSON.parse(xhttp.responseText)
+            // parse elements
+            if (response && response.length > 0) {
                 myChart.data.labels = [];
                 myChart.data.datasets[0].data = [];
-
-                newData.forEach(point => {
-                    myChart.data.labels.push(point.x);
-                    myChart.data.datasets[0].data.push(point.y);
-                });
-
-                if (newData.length > 0) {
-                    const lastValue = newData[newData.length - 1].y;
-                    document.getElementById('currentWaterLevel').innerText = lastValue;
+    
+                for (const measurement of response) {
+                    myChart.data.labels.push(measurement['value'].toString());
+                    myChart.data.datasets[0].data.push(measurement['timestamp'].toString());
                 }
-
+    
+                if (response.length > 0) {
+                    const lastValue = response[response.length - 1]['value'];
+                    document.getElementById('currentWaterLevel').innerText = String(lastValue);
+                }
+    
                 myChart.update();
-            })
-            .catch(error => console.error('Error fetching data:', error));
+            }
+        }
+        xhttp.open("GET", waterLevelIndex, true)
+        xhttp.setRequestHeader("Content-Type", "application/json")
+        xhttp.send()
     }
     setInterval(fetchData, 5000);
 
